b50e2d74302a4525bba94c5764113b5e646e30d1
Oleg Lyukshin <oleg@lyukshin.ru>
2023-02-14 14:26:17 +0300

    add Example 01 in 3 tasks

---
+++ Example01/Program.cs
@@ -0,0 +1,25 @@
+﻿// int - 
+// double - позволяет работать с вещественными числами
+// string - строки от 0 до много символов
+// bool - булев тип, true false
+
+
+// Пример использования int
+int numberA = 12;
+int numberB = 5;
+Console.WriteLine("Пример использования int");
+Console.WriteLine(numberA + numberB);
+Console.WriteLine(numberA / numberB);
+Console.WriteLine();
+
+double numberC = 12;
+double numberD = 5;
+Console.WriteLine($"Пример использования double");
+Console.WriteLine(numberC / numberD);
+Console.WriteLine();
+
+// new Random().Next(1,100) - Сгенерировать случайное число от 1 до 100
+int numberE = new Random().Next(1,100);
+int numberF = new Random().Next(1,100);
+Console.WriteLine($"Пример использования Random для числел {numberE} и {numberF}");
+Console.WriteLine(numberE + numberF);
